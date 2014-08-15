77512a4b948e9b0a3b646d3744dd301364e1c065
Bluease <roychen986rongqia@hotmail.com>
2014-08-15 13:32:52 -0400

    wp

--- firmware/1w.h
+++ firmware/1w.h
@@ -6,7 +6,7 @@
 #if ARDUINO >= 100
 #include "Arduino.h"       // for delayMicroseconds, digitalPinToBitMask, etc
 #else
-#include "WProgram.h"      // for delayMicroseconds
+//#include "WProgram.h"      // for delayMicroseconds
 #include "pins_arduino.h"  // for digitalPinToBitMask, etc
 #endif
 
